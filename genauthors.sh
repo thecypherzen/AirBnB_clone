e09dec9479869ce2e91de19b57a9289cd6580fce
William Inyam <sw.inyam@outlook.com>
2024-03-06 23:34:42 +0000

    style(hbnb:genauth): change a little bit of text

--- genauthors.sh
+++ genauthors.sh
@@ -3,7 +3,7 @@ set -e
 SPATH="$($(cd $(dirname "${BASH_SOURCE[0]}")) && pwd)"
 cat > "$SPATH/AUTHORS" <<EOF
 # This file is generated using <$(echo ${BASH_SOURCE[0]} | rev | cut -d "/" -f1 | rev)>
-# It lists the contributing authors of the gosh(go-shell) project
+# It lists the contributing authors of the AirBnB_clone
 
 $(git -C $(echo $SPATH) log --format="%aN <%aE" | LC_ALL=C.UTF-8 sort -u)
 EOF
