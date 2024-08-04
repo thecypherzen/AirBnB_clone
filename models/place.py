748a310388261bf595660f176050e47388b0ef50
William Inyam <sw.inyam@outlook.com>
2024-08-04 16:20:48 +0000

    fix(models:place): update default values

--- models/place.py
+++ models/place.py
@@ -26,12 +26,12 @@ class Place(BaseModel):
     city_id = ""
     user_id = ""
     description = ""
-    number_rooms = ""
-    number_bathrooms = ""
-    max_guest = ""
-    price_by_night = ""
-    latitude = ""
-    longitude = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = ""
 
     def __init__(self, *args, **kwargs):
